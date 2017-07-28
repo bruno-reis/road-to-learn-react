fe3d28866e94d14f0f5500906c1488b65dfd0c31
bruno-reis <13205396+bruno-reis@users.noreply.github.com>
2017-07-28 20:13:58 -0300

    added Dismiss and search form from chapter2 lessons

--- src/App.js
+++ src/App.js
@@ -20,11 +20,34 @@ const list = [
   }
 ]
 
+const isSearched = (searchTerm) => (item) =>
+  !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase())
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {list, searchTerm: ''}
+    this.onDismiss = this.onDismiss.bind(this)
+    this.onSearchChange = this.onSearchChange.bind(this)
+  }
+
+  onDismiss(id) {
+    const updatedList = this.state.list.filter(el => el.objectID !== id)
+    this.setState({list: updatedList})
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value})
+  }
+
   render() {
+    const {searchTerm, list} = this.state
     return (
       <div className="App">
-        { list.map(item =>
+        <form>
+          <input type="text" value={searchTerm} onChange={this.onSearchChange}/>
+        </form>
+        { list.filter(isSearched(searchTerm)).map(item =>
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title} </a>
@@ -32,6 +55,9 @@ class App extends Component {
             <span>{item.author} </span>
             <span>{item.num_comments} </span>
             <span>{item.points} </span>
+            <span>
+              <button onClick={() => this.onDismiss(item.objectID)} type="button">Dismiss</button>
+            </span>
           </div>
         )}
       </div>
